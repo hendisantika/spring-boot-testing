7526f4f523326cbc09aed21e3061abc23e64a358
Hendi Santika <hendisantika@gmail.com>
2021-06-18 05:16:57 +0700

    #21 - Add BookingResultResource

---
+++ src/main/java/com/hendisantika/springboottesting/booking/web/BookingResultResource.java
@@ -0,0 +1,20 @@
+package com.hendisantika.springboottesting.booking.web;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-testing
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/06/21
+ * Time: 05.16
+ */
+@Data
+@Builder
+public class BookingResultResource {
+
+    private boolean success;
+}
