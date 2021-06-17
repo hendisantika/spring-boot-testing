da926b3c46f28f306395175953effb3b99b76e75
Hendi Santika <hendisantika@gmail.com>
2021-06-18 05:36:43 +0700

    #40 - Add printApplicationContext in BookingControllerTest

--- src/test/java/com/hendisantika/springboottesting/booking/web/BookingControllerTest.java
+++ src/test/java/com/hendisantika/springboottesting/booking/web/BookingControllerTest.java
@@ -1,12 +1,15 @@
 package com.hendisantika.springboottesting.booking.web;
 
 import com.hendisantika.springboottesting.booking.business.BookingService;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,5 +32,13 @@ class BookingControllerTest {
 
     @MockBean
     private BookingService bookingService;
+
+    @BeforeEach
+    void printApplicationContext() {
+        Arrays.stream(applicationContext.getBeanDefinitionNames())
+                .map(name -> applicationContext.getBean(name).getClass().getName())
+                .sorted()
+                .forEach(System.out::println);
+    }
 
 }
