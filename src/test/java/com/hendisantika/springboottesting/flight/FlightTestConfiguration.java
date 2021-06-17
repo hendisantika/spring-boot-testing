08088e7015c4e91b99cd572240f2f9f030563eca
Hendi Santika <hendisantika@gmail.com>
2021-06-18 05:43:11 +0700

    #51 - Add FlightTestConfiguration

---
+++ src/test/java/com/hendisantika/springboottesting/flight/FlightTestConfiguration.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springboottesting.flight;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-testing
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/06/21
+ * Time: 05.42
+ */
+@SpringBootConfiguration
+@EnableAutoConfiguration
+class FlightTestConfiguration extends FlightConfiguration {
+
+}
