fb00dc0737b8d547a2690c495c1397f6f5e93195
Hendi Santika <hendisantika@gmail.com>
2021-06-17 09:07:28 +0700

    #10 - Add CustomerService

---
+++ src/main/java/com/hendisantika/springboottesting/customer/business/CustomerService.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboottesting.customer.business;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-testing
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/06/21
+ * Time: 09.07
+ */
+public class CustomerService {
+}
