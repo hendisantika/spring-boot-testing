6e0fb2ff9d0b27f043405ddd56057cb6fec065ac
Hendi Santika <hendisantika@gmail.com>
2021-06-18 05:32:52 +0700

    #36 - Add setup in BookingIntegrationTest

--- src/test/java/com/hendisantika/springboottesting/BookingIntegrationTest.java
+++ src/test/java/com/hendisantika/springboottesting/BookingIntegrationTest.java
@@ -1,9 +1,11 @@
 package com.hendisantika.springboottesting;
 
 import com.hendisantika.springboottesting.customer.data.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 /**
@@ -25,5 +27,12 @@ class BookingIntegrationTest {
     private CustomerRepository customerRepository;
 
     private MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(applicationContext)
+                .build();
+    }
 
 }
