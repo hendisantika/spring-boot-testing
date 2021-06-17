50f1ca53ee3fc64f75f31a23739f3d217474ac65
Hendi Santika <hendisantika@gmail.com>
2021-06-18 05:39:39 +0700

    #45 - Add CustomerTestConfiguration

---
+++ src/test/java/com/hendisantika/springboottesting/customer/CustomerTestConfiguration.java
@@ -0,0 +1,18 @@
+package com.hendisantika.springboottesting.customer;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-testing
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/06/21
+ * Time: 05.39
+ */
+@SpringBootConfiguration
+@EnableAutoConfiguration
+class CustomerTestConfiguration extends CustomerConfiguration {
+}
