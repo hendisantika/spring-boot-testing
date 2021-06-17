04f1ab8b049ebde188a6e6f395acbb95981a3e27
Hendi Santika <hendisantika@gmail.com>
2021-06-18 05:42:33 +0700

    #50 - Add onlyCustomerControllerIsLoaded in CustomerModuleWebLayerTests

--- src/test/java/com/hendisantika/springboottesting/customer/web/CustomerModuleWebLayerTests.java
+++ src/test/java/com/hendisantika/springboottesting/customer/web/CustomerModuleWebLayerTests.java
@@ -1,8 +1,11 @@
 package com.hendisantika.springboottesting.customer.web;
 
 import com.hendisantika.springboottesting.booking.web.BookingController;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,5 +24,11 @@ class CustomerModuleWebLayerTests {
 
     @Autowired(required = false)
     private BookingController bookingController;
+
+    @Test
+    void onlyCustomerControllerIsLoaded() {
+        assertThat(customerController).isNotNull();
+        assertThat(bookingController).isNull();
+    }
 
 }
