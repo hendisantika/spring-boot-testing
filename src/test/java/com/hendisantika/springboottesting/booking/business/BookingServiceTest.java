1090ae81ec9159ffa4f72e99461d4d230e6d986f
Hendi Santika <hendisantika@gmail.com>
2021-06-18 05:29:08 +0700

    #31 - Add bookFlightReturnsBooking Test Case in BookingServiceTest

--- src/test/java/com/hendisantika/springboottesting/booking/business/BookingServiceTest.java
+++ src/test/java/com/hendisantika/springboottesting/booking/business/BookingServiceTest.java
@@ -1,10 +1,17 @@
 package com.hendisantika.springboottesting.booking.business;
 
+import com.hendisantika.springboottesting.booking.data.Booking;
 import com.hendisantika.springboottesting.booking.data.BookingRepository;
 import com.hendisantika.springboottesting.customer.data.CustomerRepository;
 import com.hendisantika.springboottesting.flight.data.FlightService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,5 +34,17 @@ class BookingServiceTest {
     @BeforeEach
     void setup() {
         this.bookingService = new BookingService(bookingRepository, customerRepository, flightService);
+    }
+
+    @Test
+    void bookFlightReturnsBooking() {
+        when(customerRepository.findById(42L)).thenReturn(customer());
+        when(flightService.findFlight("Oceanic 815")).thenReturn(flight());
+        when(bookingRepository.save(eq(booking()))).thenReturn(booking());
+        Booking booking = bookingService.bookFlight(42L, "Oceanic 815");
+
+        assertThat(booking).isNotNull();
+        verify(bookingRepository).save(eq(booking));
+
     }
 }
