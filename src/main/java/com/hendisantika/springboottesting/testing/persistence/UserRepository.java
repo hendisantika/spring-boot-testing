c96ac4e0d512e1699a9e1bcd91bfe8e0f89ce2a6
Hendi Santika <hendisantika@gmail.com>
2021-06-18 05:51:09 +0700

    #62 - Add others method in UserRepository

--- src/main/java/com/hendisantika/springboottesting/testing/persistence/UserRepository.java
+++ src/main/java/com/hendisantika/springboottesting/testing/persistence/UserRepository.java
@@ -1,6 +1,8 @@
 package com.hendisantika.springboottesting.testing.persistence;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Created by IntelliJ IDEA.
@@ -12,5 +14,11 @@ import org.springframework.data.repository.CrudRepository;
  * Time: 05.50
  */
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
+    UserEntity findByName(String name);
 
+    @Query("select u from UserEntity u where u.name = :name")
+    UserEntity findByNameCustomQuery(@Param("name") String name);
+
+    @Query(value = "select * from user as u where u.name = :name", nativeQuery = true)
+    UserEntity findByNameNativeQuery(@Param("name") String name);
 }
