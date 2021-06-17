03050d310b418921d38508dfdd21cd0bb4e8af40
Hendi Santika <hendisantika@gmail.com>
2021-06-18 05:19:49 +0700

    #24 - Add BookingConfiguration

--- src/main/java/com/hendisantika/springboottesting/booking/BookingConfiguration.java
+++ src/main/java/com/hendisantika/springboottesting/booking/BookingConfiguration.java
@@ -1,7 +1,15 @@
 package com.hendisantika.springboottesting.booking;
 
+import com.hendisantika.springboottesting.booking.business.BookingService;
+import com.hendisantika.springboottesting.booking.data.BookingRepository;
+import com.hendisantika.springboottesting.customer.CustomerConfiguration;
+import com.hendisantika.springboottesting.customer.data.CustomerRepository;
+import com.hendisantika.springboottesting.flight.FlightConfiguration;
+import com.hendisantika.springboottesting.flight.data.FlightService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,16 +21,16 @@ import org.springframework.context.annotation.Configuration;
  * Time: 09.01
  */
 @Configuration
-//@Import({CustomerConfiguration.class, FlightConfiguration.class})
+@Import({CustomerConfiguration.class, FlightConfiguration.class})
 @ComponentScan
 public class BookingConfiguration {
 
-//    @Bean
-//    public BookingService bookingService(
-//            BookingRepository bookingRepository,
-//            CustomerRepository customerRepository,
-//            FlightService flightService) {
-//        return new BookingService(bookingRepository, customerRepository, flightService);
-//    }
+    @Bean
+    public BookingService bookingService(
+            BookingRepository bookingRepository,
+            CustomerRepository customerRepository,
+            FlightService flightService) {
+        return new BookingService(bookingRepository, customerRepository, flightService);
+    }
 
 }
