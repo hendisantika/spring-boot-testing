23f3ecd6114c891c8af30b8b2b5edc67766b6d31
Hendi Santika <hendisantika@gmail.com>
2021-06-18 05:33:27 +0700

    #37 - Add bookFlightReturnsHttpStatusOk in BookingIntegrationTest

--- src/test/java/com/hendisantika/springboottesting/BookingIntegrationTest.java
+++ src/test/java/com/hendisantika/springboottesting/BookingIntegrationTest.java
@@ -1,12 +1,16 @@
 package com.hendisantika.springboottesting;
 
+import com.hendisantika.springboottesting.customer.data.Customer;
 import com.hendisantika.springboottesting.customer.data.CustomerRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 /**
  * Created by IntelliJ IDEA.
@@ -33,6 +37,19 @@ class BookingIntegrationTest {
         this.mockMvc = MockMvcBuilders
                 .webAppContextSetup(applicationContext)
                 .build();
+    }
+
+    @Test
+    void bookFlightReturnsHttpStatusOk() throws Exception {
+        this.customerRepository.save(Customer.builder()
+                .name("Naruto")
+                .build());
+
+        this.mockMvc.perform(
+                post("/booking")
+                        .param("customerId", "1")
+                        .param("flightNumber", "Oceanic 815"))
+                .andExpect(status().isOk());
     }
 
 }
