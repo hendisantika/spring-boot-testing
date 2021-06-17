94ef85be3c3d344ce3cca9c18525ad907b8b987c
Hendi Santika <hendisantika@gmail.com>
2021-06-17 09:15:56 +0700

    #18 - Add BookingService

---
+++ src/main/java/com/hendisantika/springboottesting/booking/business/BookingService.java
@@ -0,0 +1,23 @@
+package com.hendisantika.springboottesting.booking.business;
+
+import com.hendisantika.springboottesting.booking.data.BookingRepository;
+import com.hendisantika.springboottesting.customer.data.CustomerRepository;
+import com.hendisantika.springboottesting.flight.data.FlightService;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-testing
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/06/21
+ * Time: 09.15
+ */
+public class BookingService {
+    private BookingRepository bookingRepository;
+
+    private CustomerRepository customerRepository;
+
+    private FlightService flightService;
+
+}
