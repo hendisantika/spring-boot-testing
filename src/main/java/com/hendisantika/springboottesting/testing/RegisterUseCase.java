87aa7d4664b29196d99f668b3e0b50dfe0c2cc04
Hendi Santika <hendisantika@gmail.com>
2021-06-18 05:46:40 +0700

    #53 - Add SaveUserPort

---
+++ src/main/java/com/hendisantika/springboottesting/testing/RegisterUseCase.java
@@ -0,0 +1,33 @@
+package com.hendisantika.springboottesting.testing;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-testing
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/06/21
+ * Time: 05.45
+ */
+@Service
+@RequiredArgsConstructor
+public class RegisterUseCase {
+
+//    private final SaveUserPort saveUserPort;
+//
+//    private final SendMailPort sendMailPort;
+//
+//    public Long registerUser(User user, boolean sendWelcomeMail) {
+//        user.setRegistrationDate(LocalDateTime.now());
+//
+//        if(sendWelcomeMail){
+//            sendMailPort.sendMail("Welcome!", "Thanks for registering.");
+//        }
+//
+//        return saveUserPort.saveUser(user);
+//    }
+
+}
