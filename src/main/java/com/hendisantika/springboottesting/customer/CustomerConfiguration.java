cb440af7565e8dbc740695ac436fa2928f2965d6
Hendi Santika <hendisantika@gmail.com>
2021-06-17 09:06:01 +0700

    #7 - Add CustomerConfiguration

---
+++ src/main/java/com/hendisantika/springboottesting/customer/CustomerConfiguration.java
@@ -0,0 +1,24 @@
+package com.hendisantika.springboottesting.customer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-testing
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/06/21
+ * Time: 09.03
+ */
+@Configuration
+@ComponentScan
+public class CustomerConfiguration {
+
+    @Bean
+    public CustomerService customerService() {
+        return new CustomerService();
+    }
+}
