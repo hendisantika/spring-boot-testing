83bb83526aac67a9eafd8c87b013bef0c6481668
Hendi Santika <hendisantika@gmail.com>
2021-06-18 05:25:37 +0700

    #28 - Add config test

--- src/test/java/com/hendisantika/springboottesting/SpringBootTestingApplicationTests.java
+++ src/test/java/com/hendisantika/springboottesting/SpringBootTestingApplicationTests.java
@@ -1,9 +1,16 @@
 package com.hendisantika.springboottesting;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(properties = {
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.liquibase.enabled=false",
+        "spring.flyway.enabled=false"
+})
 class SpringBootTestingApplicationTests {
 
     @Test
