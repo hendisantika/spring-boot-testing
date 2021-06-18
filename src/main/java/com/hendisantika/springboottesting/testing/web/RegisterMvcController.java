28ba01dc502af6689dbe9063f1db13c42693c6cf
Hendi Santika <hendisantika@gmail.com>
2021-06-19 05:50:21 +0700

    #68 - Add register in RegisterMvcController

--- src/main/java/com/hendisantika/springboottesting/testing/web/RegisterMvcController.java
+++ src/main/java/com/hendisantika/springboottesting/testing/web/RegisterMvcController.java
@@ -1,8 +1,12 @@
 package com.hendisantika.springboottesting.testing.web;
 
 import com.hendisantika.springboottesting.testing.RegisterUseCase;
+import com.hendisantika.springboottesting.testing.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +22,19 @@ import org.springframework.stereotype.Controller;
 class RegisterMvcController {
 
     private final RegisterUseCase registerUseCase;
+
+    @PostMapping("/mvc/register/")
+    ModelAndView register(@ModelAttribute("user") UserResource userResource) {
+
+        User user = new User(
+                userResource.getName(),
+                userResource.getEmail());
+
+        registerUseCase.registerUser(user, false);
+
+        ModelAndView modelAndView = new ModelAndView("registration_success.html");
+        modelAndView.addObject("username", user.getName());
+
+        return modelAndView;
+    }
 }
