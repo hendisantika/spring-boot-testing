5df2fc4af62ad55ecba4f4e0713ca8e1819ca00c
Hendi Santika <hendisantika@gmail.com>
2021-06-19 05:53:08 +0700

    #70 - Add register in RegisterRestController

--- src/main/java/com/hendisantika/springboottesting/testing/web/RegisterRestController.java
+++ src/main/java/com/hendisantika/springboottesting/testing/web/RegisterRestController.java
@@ -1,8 +1,15 @@
 package com.hendisantika.springboottesting.testing.web;
 
 import com.hendisantika.springboottesting.testing.RegisterUseCase;
+import com.hendisantika.springboottesting.testing.domain.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +25,20 @@ import org.springframework.web.bind.annotation.RestController;
 class RegisterRestController {
 
     private final RegisterUseCase registerUseCase;
+
+    @PostMapping("/forums/{forumId}/register")
+    UserResource register(
+            @PathVariable("forumId") Long forumId,
+            @Valid @RequestBody UserResource userResource,
+            @RequestParam("sendWelcomeMail") boolean sendWelcomeMail) {
+
+        User user = new User(
+                userResource.getName(),
+                userResource.getEmail());
+        Long userId = registerUseCase.registerUser(user, sendWelcomeMail);
+
+        return new UserResource(
+                user.getName(),
+                user.getEmail());
+    }
 }
