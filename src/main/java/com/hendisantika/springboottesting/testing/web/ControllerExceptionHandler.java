25ae1acc944be816ca11bac3b849bfa70e749ee4
Hendi Santika <hendisantika@gmail.com>
2021-06-19 05:48:45 +0700

    #65 - Add ControllerExceptionHandler

---
+++ src/main/java/com/hendisantika/springboottesting/testing/web/ControllerExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.hendisantika.springboottesting.testing.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-testing
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/06/21
+ * Time: 05.46
+ */
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public ErrorResult handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        ErrorResult errorResult = new ErrorResult();
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            errorResult.getFieldErrors()
+                    .add(new FieldValidationError(fieldError.getField(), fieldError.getDefaultMessage()));
+        }
+        return errorResult;
+    }
+}
