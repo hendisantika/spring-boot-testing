4cc6094de14458b692e5643e9dbe557a737287de
Hendi Santika <hendisantika@gmail.com>
2021-06-19 05:59:04 +0700

    #74 - Add registrationWorksThroughAllLayers in RegisterUseCaseIntegrationTest

--- src/test/java/com/hendisantika/springboottesting/testing/RegisterUseCaseIntegrationTest.java
+++ src/test/java/com/hendisantika/springboottesting/testing/RegisterUseCaseIntegrationTest.java
@@ -1,11 +1,18 @@
 package com.hendisantika.springboottesting.testing;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hendisantika.springboottesting.testing.persistence.UserEntity;
 import com.hendisantika.springboottesting.testing.persistence.UserRepository;
+import com.hendisantika.springboottesting.testing.web.UserResource;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by IntelliJ IDEA.
@@ -32,4 +39,18 @@ public class RegisterUseCaseIntegrationTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Test
+    void registrationWorksThroughAllLayers() throws Exception {
+        UserResource user = new UserResource("Uzumaki Naruto", "uzumaki_naruto@konohagakure.co.jp");
+
+        mockMvc.perform(post("/forums/{forumId}/register", 42L)
+                .contentType("application/json")
+                .param("sendWelcomeMail", "true")
+                .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isOk());
+
+        UserEntity userEntity = userRepository.findByName("Uzumaki Naruto");
+        assertThat(userEntity.getEmail()).isEqualTo("uzumaki_naruto@konohagakure.co.jp");
+    }
 }
