fcba5e2d4d6ee033a6819e36a332899855ad0516
Hendi Santika <hendisantika@gmail.com>
2021-06-19 06:05:25 +0700

    #81 - Add SpringBootPropertiesTest

---
+++ src/test/java/com/hendisantika/springboottesting/testing/SpringBootPropertiesTest.java
@@ -0,0 +1,28 @@
+package com.hendisantika.springboottesting.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-testing
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/06/21
+ * Time: 06.04
+ */
+@SpringBootTest(properties = "foo=bar")
+class SpringBootPropertiesTest {
+
+    @Value("${foo}")
+    String foo;
+
+    @Test
+    void test() {
+        assertThat(foo).isEqualTo("bar");
+    }
+}
