31ae5020963c64891c3277cfe7dfee68b5dabba6
Hendi Santika <hendisantika@gmail.com>
2021-06-19 05:57:43 +0700

    #73 - Add RegisterUseCaseIntegrationTest

---
+++ src/test/java/com/hendisantika/springboottesting/testing/RegisterUseCaseIntegrationTest.java
@@ -0,0 +1,35 @@
+package com.hendisantika.springboottesting.testing;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hendisantika.springboottesting.testing.persistence.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-testing
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/06/21
+ * Time: 05.57
+ */
+@SpringBootTest(properties = {
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.liquibase.enabled=false",
+        "spring.flyway.enabled=false"
+})
+@AutoConfigureMockMvc
+public class RegisterUseCaseIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private UserRepository userRepository;
+}
